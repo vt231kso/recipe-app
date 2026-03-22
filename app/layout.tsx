eef84a2c50c0718af3a33b6227460bca45fc5950
vt231_kso <vt231_kso@student.ztu.edu.ua>
2026-03-22 11:42:18 +0200

    fixed erros

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,16 @@
 import HeaderTop from "@/components/Header";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import {Suspense} from "react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="uk">
     <body className="bg-[#FCFBF7] text-gray-900 antialiased">
     <header className="sticky top-0 z-50 shadow-sm bg-white">
+      <Suspense fallback={<div className="h-[60px] bg-white animate-pulse" />}>
       <HeaderTop />
+      </Suspense>
       <Navbar />
     </header>
 
